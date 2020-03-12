fe5f84e62f6b9eb9c5f4c212328b167d52d85aba
Cody Brubaker <cody.brubaker@gmail.com>
2020-03-12 12:48:59 -0400

    work on admin section

--- admin.php
+++ admin.php
@@ -14,7 +14,9 @@ switch ($fnc) {
 	case 'addPlayer':
 		admin::addPlayer();
 		break;
-	
+	case 'resetGame':
+		admin::resetGame();
+		break;
 	default:
 		admin::defaultRun();
 		break;
@@ -23,31 +25,60 @@ switch ($fnc) {
 class admin {
 
 	function defaultRun() {
-	
-		include "admin.html";
+		$mysqli = admin::connectToDB();
 
+		$sql = "SELECT * FROM game_players";
+		$statement = $mysqli->prepare($sql);
+		// $result = $statement->execute();
+		$result = $mysqli->query($sql);
+
+		$num_players = $result->affected_rows;
+		error_log("NUM: ".$num_players);
+		while ($row = $result->fetch_assoc()){
+			error_log(print_r($row, true));
+		}
+		include "admin.html";
 	}
 
 	function addPlayer() {
 		error_log('adding');
 		error_log(print_r($_POST, true));
-		admin::connectToDB();
+		$mysqli = admin::connectToDB();
+
+		$sql = "INSERT INTO game_players (player_name)
+				VALUES (?)";
+		$statement = $mysqli->prepare($sql);
+		$statement->bind_param(
+			's',
+			$_POST['player']
+		);
+
+		$result = $statement->execute();
 	}
 
+	function resetGame() {
+		error_log('reset');
+		$mysqli = admin::connectToDB();
+
+		$sql = "DELETE FROM game_players";
+		$statement = $mysqli->prepare($sql);
+
+		$result = $statement->execute();
+	}
 
 	function connectToDB() {
 		define('DB_HOST',"127.0.0.1:3306");
 		define('DB_NAME',"leaderboard");
-		define('DB_USER',"root");
-		define('DB_PASS',"N2D{ZbXyrPWv");
-		error_log('CONNECTING');
+		define('DB_USER',"golf");
+		define('DB_PASS',"Golf-champ-2020");
+
 		$mysqli_db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-		if ($mysqli -> connect_errno) {
+		if ($mysqli_db -> connect_errno) {
 			echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
 			exit();
 		}
-		error_log('SUCCESS');
+
 		return $mysqli_db;
 	}
 }
