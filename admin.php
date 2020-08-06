21111b9a91b82d074897b55ec6503a5163c54721
Cody Brubaker <cody.brubaker@gmail.com>
2020-08-05 22:56:56 -0400

    assign golfers and list them under each player

--- admin.php
+++ admin.php
@@ -3,19 +3,22 @@
 ini_set('error_log', '/tmp/leaderboard.log');
 
 $fnc = $_GET['event'];
-error_log(print_r($_GET, true));
+
 $http_proto = 'http';
 $base_url = $http_proto.'://'.$_SERVER['HTTP_HOST'];
 
 define('BASE_URL', $base_url);
 define('APP_DIR', getcwd());
-error_log('running file');
+
 switch ($fnc) {
 	case 'addPlayer':
 		admin::addPlayer();
 		break;
 	case 'resetGame':
 		admin::resetGame();
+		break;
+	case 'addGolfer';
+		admin::addGolfer();
 		break;
 	default:
 		admin::defaultRun();
@@ -28,23 +31,44 @@ class admin {
 		$mysqli = admin::connectToDB();
 
 		$sql = "SELECT * FROM game_players";
-		// $statement = $mysqli->prepare($sql);
-		// $result = $statement->execute();
 		$result = $mysqli->query($sql);
-
 		$num_players = $result->num_rows;
-		error_log("NUM: ".$num_players);
+
+		// get list of golfers
+		$golfers_html = admin::getGolfers();
+		// error_log($golfers_html);
+
 		$i = 1;
 		$players = '';
 		while ($row = $result->fetch_assoc()){
-			error_log(print_r($row, true));
 			if ($i % 2 != 0) {
 				$players .= '<div class="row">';
 			}
 
+			// get assigned golfers
+			$sql2 = "SELECT * 
+					FROM assigned_golfers
+					WHERE player_id = ?";
+			$statement = $mysqli->prepare($sql2);
+			$statement->bind_param(
+				'i',
+				$row['player_id']
+			);
+	
+			$statement->execute();
+			$result2 = $statement->get_result();
+			// $result = $mysqli->query($sql);
+			$list = '<ul>';
+			while ($golfer = $result2->fetch_assoc()) {
+				error_log(print_r($golfer, true));
+				$list .= '<li>'.$golfer['golfer_name'].'</li>';
+			}
+			$list .= '</ul>';
+
 			$players .= '<div class="card col-sm">
 				<div class="card-body">
-					<h6>'.$row['player_name'].'</h6>
+					<h3>'.$row['player_name'].'</h3>
+					'.$list.'
 					<button type="button" class="btn-small player-val" data-toggle="modal" data-target="#exampleModal" value="'.$row['player_id'].'">Add Golfer</button>
 				</div>
 			</div>';
@@ -63,6 +87,57 @@ class admin {
 		include "admin.html";
 	}
 
+	function getGolfers() {
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => "http://site.api.espn.com/apis/site/v2/sports/golf/leaderboard",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_POSTFIELDS => "",
+		CURLOPT_HTTPHEADER => array(
+			"Accept: application/json",
+			"Cache-Control: no-cache",
+			"Connection: keep-alive",
+			"Content-Type: application/json",
+			"Host: site.api.espn.com",
+			"User-Agent: PostmanRuntime/7.11.0",
+			"accept-encoding: gzip, deflate",
+			"cache-control: no-cache",
+			"cookie: SWID=5585BF87-D6BE-4E7F-CE7B-073F0CE0ADDE"
+		),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+
+		if ($err) {
+			echo $err;
+		} else {
+			$data = json_decode($response, true);
+			$competitors = $data['events'][0]['competitions'][0]['competitors'];
+
+			$golfers_html = "<select name='golfer-name' id='golfer-name'>";
+			for ($i=0; $i < count($competitors); $i++) {
+				// code...
+				$name = $competitors[$i]['athlete']['displayName'];
+
+				$golfers_html .= "<option value='".$name."'>".$name."</option>";
+
+			}
+			$golfers_html .= "</select>";
+		}	
+
+
+		return $golfers_html;
+	}
+
 	function addPlayer() {
 		error_log('adding');
 		error_log(print_r($_POST, true));
@@ -77,7 +152,6 @@ class admin {
 		);
 
 		$result = $statement->execute();
-		error_log('PLAYER ADDED');
 	}
 
 	function resetGame() {
@@ -86,6 +160,24 @@ class admin {
 
 		$sql = "DELETE FROM game_players";
 		$statement = $mysqli->prepare($sql);
+
+		$result = $statement->execute();
+	}
+
+	function addGolfer() {
+		error_log('addGolfer()');
+		error_log(print_r($_POST, true));
+
+		$mysqli = admin::connectToDB();
+
+		$sql = "INSERT INTO assigned_golfers (golfer_name, player_id)
+				VALUES (?, ?)";
+		$statement = $mysqli->prepare($sql);
+		$statement->bind_param(
+			'si',
+			$_POST['golfer'],
+			$_POST['player_id']
+		);
 
 		$result = $statement->execute();
 	}
